358c5aad2d1afe45e48e54247a00bc1a167c3970
s-ozono <s_ozono@skyleaf.co.jp>
2026-01-23 21:43:22 +0900

    Commit.

---
+++ java演習問題提出用/tsuika_ensyu3.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TSUIKA_ENSYU3 {
+	
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		
+		Scanner fileScanner = null;
+		
+		try {
+			
+			//ファイルのオブジェクトを生成
+			File putFile = new File(args[0]);
+			
+			// UTF-8 形式のテキストファイルを読み込む
+			fileScanner = new Scanner(putFile, "UTF-8");
+			
+			//テキストファイルに記載された内容を list に格納
+			while(fileScanner.hasNextLine()) {
+				
+				list.add(fileScanner.nextLine());
+			}
+			
+			//テキストファイルの1行目から順に出力
+			for(String line : list) {
+				
+				System.out.println(line);
+			}
+			
+			//テキストファイルの最終行から逆順に1行目まで出力
+			for(int i = list.size(); i > 0; i--) {
+				
+				System.out.println(list.get(i));
+			}
+			
+			//テキストファイルのうち、「あいうえお」の文字列が格納されている行番号を出力
+			for(int i = 0; i < list.size(); i++) {
+				
+				if(list.get(i).equals("あいうえお")) {
+					
+					System.out.println(i + 1 + "行目");
+				}
+			}
+			
+		} catch(Exception exception) {
+			
+			System.out.println("何らかの例外が発生しました");
+		
+		} finally {
+			
+			//ファイルを閉じる
+			fileScanner.close();
+		}
+		
+	}
+}		
+	
+		
