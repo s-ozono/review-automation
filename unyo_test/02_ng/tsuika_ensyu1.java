73d49307bfbe90db3927ebb551defbb913850157
s-ozono <s_ozono@skyleaf.co.jp>
2026-04-24 01:39:38 +0900

    Commit.

---
+++ unyo_test/02_ng/tsuika_ensyu1.java
@@ -0,0 +1,48 @@
+public class TSUIKA_ENSYU1_NG {
+
+	public static void main(String[] args) {
+
+		// NullPointerExceptionをthrowするが原因処理は書かれていない
+		try {
+
+			throw new NullPointerException();
+
+		} catch (Exception e) {
+
+			// 詳細情報を出力しない
+			System.out.println("例外発生");
+
+			// 再スローして異常終了させる
+			throw e;
+
+		}
+
+		// ArrayIndexOutOfBoundsExceptionをthrowするが原因処理は書かれていない
+		try {
+
+			throw new ArrayIndexOutOfBoundsException();
+
+		} catch (Exception e) {
+
+			System.out.println("例外発生");
+
+			throw e;
+
+		}
+
+		// ClassCastExceptionをthrowするが原因処理は書かれていない
+		try {
+
+			throw new ClassCastException();
+
+		} catch (Exception e) {
+
+			System.out.println("例外発生");
+
+			throw e;
+
+		}
+
+	}
+
+}
